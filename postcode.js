e2f743e839db81e04706c225b822b71feb14e1a8
Sri Laasya Nutheti <nutheti.laasya@gmail.com>
2021-02-22 01:47:17 +0530

    JS changes and fixed buttons

--- postcode.js
+++ postcode.js
@@ -1,5 +1,5 @@
 function postcode() {
     var client = new WebTorrent();
     var file = new File([document.all('send').value], document.all('name').value);
-    client.seed(file, function(torrent){ document.all('deliver').innerHTML = 'Link to posted file:\nhttp://call-me-bk.github.io/text-bucket/?' + torrent.infoHash });
+    client.seed(file, function(torrent){ document.all('deliver').innerHTML = 'Shareable link:\nhttp://call-me-bk.github.io/text-bucket/?' + torrent.infoHash });
   }
